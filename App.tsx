00d3cf0e1d9d9b2d27e6982ecb0ca219f2f75a8e
Sidney Alexandre Pereira Martins <smartins@daitan.com>
2020-08-09 23:50:53 -0300

    Main Navigation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/Landing/index';
 import { AppLoading } from 'expo';
+import AppStack from './src/routes/AppStack';
 import {
   Archivo_400Regular,
   Archivo_700Bold,
@@ -25,7 +25,7 @@ export default function App() {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style='light' />
       </>
     );
